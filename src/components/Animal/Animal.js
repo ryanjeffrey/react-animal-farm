5f69f1ade890957e8e0b225081937f6c85eff4e1
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-12 17:12:32 -0500

    Update dynamic link to animal imgs

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -3,7 +3,7 @@ import './Animal.css';
 export default function Animal(props) {
   return <>
     <div className="animal" style={{ top: `${props.top}`, left: `${props.left}` }}>
-      <img src={`${process.env.PUBLIC_URL}/${props.type}.svg`} />
+      <img src={process.env.PUBLIC_URL + `/animals/${props.type}.svg`} />
       <p className="name">{props.name}</p>
       <p>{props.says}</p>
     </div>
