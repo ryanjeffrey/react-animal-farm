459016b2daf0539ad7046b19efc5454d2d7fd547
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-12 16:55:41 -0500

    Set background image

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Main.css';
 import Animal from '../Animal/Animal';
 import { animals } from '../../data';
+import background from '../../background.png';
 
 export default function Main() {
-  return <main>
+  return <main style={{ backgroundImage: `url(${background})` }}>
     {animals.map((animal) => (
       <Animal key={animal.name} name={animal.name} says={animal.says} type={animal.type} />
     ))}
