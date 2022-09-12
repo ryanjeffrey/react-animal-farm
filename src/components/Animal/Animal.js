5ea98cf70103b61a8ecb902c0f174001f3e64a03
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-12 17:00:44 -0500

    Dynamically set style attribute to position animals

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,6 +1,8 @@
+import './Animal.css';
+
 export default function Animal(props) {
   return <>
-    <div className="animal">
+    <div className="animal" style={{ top: `${props.top}`, left: `${props.left}` }}>
       <img src={`${process.env.PUBLIC_URL}/${props.type}.svg`} />
       <p className="name">{props.name}</p>
       <p>{props.says}</p>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,7 +7,7 @@ import background from '../../background.png';
 export default function Main() {
   return <main style={{ backgroundImage: `url(${background})` }}>
     {animals.map((animal) => (
-      <Animal key={animal.name} name={animal.name} says={animal.says} type={animal.type} />
+      <Animal key={animal.name} name={animal.name} says={animal.says} type={animal.type} top={animal.top} left={animal.left} />
     ))}
   </main>;
 }
