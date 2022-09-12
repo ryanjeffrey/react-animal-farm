c1e0bca6f8c44efacbfce0d9db591220c897468f
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-12 16:49:17 -0500

    Create Animal component

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,9 @@
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return <>
+    <div className="animal">
+      <img src={`${process.env.PUBLIC_URL}/${props.type}.svg`} />
+      <p className="name">{props.name}</p>
+      <p>{props.says}</p>
+    </div>
+  </>;
 }
