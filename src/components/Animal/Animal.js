7f05c686341e759c484c6e6628d870cf8cb0a26b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-12 18:07:31 -0500

    Play animal sound when user clicks on an animal

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,8 +1,14 @@
 import './Animal.css';
 
 export default function Animal(props) {
+  let audio = new Audio(process.env.PUBLIC_URL + `/animals/${props.type}.mp3`);
+
+  const start = () => {
+    audio.play();
+  };
+
   return <>
-    <div className="animal" style={{ top: `${props.top}`, left: `${props.left}` }}>
+    <div className="animal" style={{ top: `${props.top}`, left: `${props.left}` }} onClick={start}>
       <img src={process.env.PUBLIC_URL + `/animals/${props.type}.svg`} />
       <p className="name">{props.name}</p>
       <p>{props.says}</p>
